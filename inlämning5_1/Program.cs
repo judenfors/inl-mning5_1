9466fe5a13399e163ae2af7376913e812f0e035f
Mopa01 <111962656+Dungeater1337@users.noreply.github.com>
2023-01-20 10:16:50 +0100

    Add project files.

---
+++ inlämning5_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace inlämning5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentences = { "Mening1", "Mening2", "Mening3", "Mening4" };
+
+            for(int i = 0; i < sentences.Length; i++)
+            {
+                Console.WriteLine(sentences[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
